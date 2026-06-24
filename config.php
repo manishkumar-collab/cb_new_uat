9a62957b15e35bdf06374dda39ccd7869be11de5
Manish-IT <manishkumar@coveryou.in>
2026-06-24 15:14:42 +0530

    config password change

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 // Database configuration
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'visitor_db');
-define('DB_USER', 'mani_user');
-define('DB_PASS', 'StrongPassword@123');
+define('DB_USER', 'root');
+define('DB_PASS', '');
 
 // Create connection
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
